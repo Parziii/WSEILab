87c72ef22bb0386037159865d7768c0d21070c8c
Dominik Sencio <dominik.sencio@microsoft.wsei.edu.pl>
2021-10-10 13:05:34 +0200

    Changes from secound lab

---
+++ Models/ProductStatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSEILab.Models
+{
+    public class ProductStatsViewModel
+    {
+        public int NameLength { get; set; }
+        public int DescriptionLength { get; set; }
+    }
+}
