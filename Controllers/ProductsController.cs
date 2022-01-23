fcefa08e1730933c2439e37d6f0a3739e50ebca8
Dominik Sencio <dominik.sencio@microsoft.wsei.edu.pl>
2022-01-23 20:54:07 +0100

    add database support

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WSEILab.Database;
+using WSEILab.Entities;
 using WSEILab.Models;
 
 
@@ -10,13 +12,29 @@ namespace WSEILab.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly AppDbContext _dbContext;
+        public ProductsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Add(ProductModel product)
+        public async Task<IActionResult> Add(ProductModel product)
         {
+            var entity = new ProductEntity
+            {
+                Name = product.Name,
+                Description = product.Description,
+                IsVisible = product.IsVisible,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
             var viewModel = new ProductStatsViewModel
             {
                 NameLength = product.Name.Length,
